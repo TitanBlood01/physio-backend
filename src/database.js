87269867d81216f81ac8571c7aae44020897d046
jose <jernestocastro01@gmail.com>
2024-12-28 07:49:39 -0400

    reorganizar database

--- src/database.js
+++ src/database.js
@@ -6,5 +6,8 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/physioActiveDB"
 
 mongoose.connect(MONGO_URI)
-    .then(db => console.log('DB is connected'))
-    .catch(error => console.log(error))
+    .then(
+        db => console.log('DB is connected')
+    ).catch(
+        error => console.log(error)
+    )
